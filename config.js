aa8002b1bbaa96fe234a2b855f11e2d95a2156d1
mk69ggg <125274859+mk69ggg@users.noreply.github.com>
2023-05-09 10:48:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? 'default' : process.env.ALIVE_MSG,
-LANG: process.env.LANG === undefined ? 'EN' : process.env.LANG,
+LANG: process.env.LANG === undefined ? 'SI' : process.env.LANG,
 ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i.imgur.com/v8iuP8v.jpg' : process.env.ALIVE_LOGO,
 FOOTER: process.env.FOOTER === undefined ? 'ᴀꜱᴛʀᴏ-ᴍᴅ ᴘᴜʙʟɪᴄ ꜱᴛᴀʙʟᴇ' : process.env.FOOTER,
 CAPTION: process.env.CAPTION === undefined ? '*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ᴀꜱᴛʀᴏ-ᴍᴅ*' : process.env.CAPTION,
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed' : process.env.INBOX_BL_MSG,
 HOST: process.env.HOST === undefined ? '-' : process.env.HOST,
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID
+SESSION_ID: process.env.SESSION_ID === undefined ? 'ASTRO=Y6Jk0YKB#E5Guj_dPXB7cB40YNVz_Ax-ez525zP-P-7zmgE2pihk' : process.env.SESSION_ID
 };
